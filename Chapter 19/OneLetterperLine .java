83a0224bb97a6b96de86bde037202011f6326fa8
Haste04 <151770888+Haste04@users.noreply.github.com>
2024-03-26 14:36:02 +0800

    Create OneLetterperLine .java

---
+++ Chapter 19/OneLetterperLine .java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class OneLetterperLine 
+{
+    public static void main (String[] args)
+    {
+        Scanner scan= new Scanner (System.in);
+        String word;
+        int times;
+        int i = 0;
+
+        System.out.printf("Enter a word: ");
+        word = scan.nextLine();
+        times = word.length();
+
+        while (i <= times)
+        {
+            System.out.println(word.charAt(i));
+            i++;
+        }
+    }
+}
+Mon 9:40 PM
+You sent
